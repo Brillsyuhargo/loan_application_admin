f5c5a2ab7e0c128c73bd290ecdda74d40608e6ed
azmx <muhammadazzamaqiladafffa@gmail.com>
2025-04-22 08:20:36 +0700

    `Refactor Simulation Calculator: Update logic_calculator.dart and remove simulation_admin.dart`

--- lib/navbar/admin/dashboard_page_admin.dart
+++ lib/navbar/admin/dashboard_page_admin.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:loan_application_admin/views/History/history_admin.dart';
 import 'package:loan_application_admin/views/Profile/profile_admin.dart';
-import 'package:loan_application_admin/views/Simulation_Calculator/simulation_admin.dart';
+import 'package:loan_application_admin/views/Simulation_Calculator/simulation_employee.dart';
 import 'package:loan_application_admin/views/SurveyList/surveylist_admin.dart';
 import 'package:loan_application_admin/controller/admin/controller_page_admin.dart';
 
@@ -59,31 +59,3 @@ class DashboardPageAdmin extends StatelessWidget {
     });
   }
 }
-
-// //BottomNavigationBar(
-//           currentIndex: dashboardController.selectedIndex.value,
-//           onTap: (index) {
-//             dashboardController.selectedIndex.value = index;
-//           },
-//           selectedItemColor: Colors.blue,
-//           unselectedItemColor: const Color.fromARGB(255, 255, 255, 255),
-//           backgroundColor: Colors.grey[850],
-//           items: const [
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.home_outlined),
-//               label: "SurveyList",
-//             ),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.bookmark),
-//               label: "History",
-//             ),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.bookmark),
-//               label: "Simulation",
-//             ),
-//             BottomNavigationBarItem(
-//               icon: Icon(Icons.person),
-//               label: "Profile",
-//             ),
-//           ],
-//         ),
